8ce8b80c6b1c26eed718234189d5671bbe7b727b
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-08-19 14:21:23 -0500

    Update HelloWorld.java

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,11 +1,7 @@
 package com.csc;
 
-// Your code goes here!
-
-package com.csc;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+        System.out.println("Hello World!");
     }
 }
